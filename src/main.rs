19fd99d20360204dd5144c59240f08f5940bd6cd
Bart Massey <bart@cs.pdx.edu>
2018-06-03 23:16:25 -0700

    moved stems to constant at top of source

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,21 @@
 // Please see the file COPYING in the source
 // distribution of this software for license terms.
 
-// Anagram generator
+//! Anagram generator.
+
+/// Stems are extra fragments used to allow more anagrams
+/// to be made by the user by giving the opportunity to glue
+/// them to words. A stemmed dictionary would be better than
+/// this plan; also, this is very English-specific.
+const STEMS:&[&str] = &[
+    "s",
+    "ed",
+    "er",
+    "ing",
+    "ly",
+    "i",
+    "a",
+];
 
 use std::env::*;
 use std::fs::File;
@@ -13,6 +27,7 @@ use std::path::*;
 
 extern crate multiset;
 
+/// Read the word list from some dictionary.
 fn open_dict() -> File {
     for file in ["scowl.txt", "eowl.txt", "words"].iter() {
         for dir in ["/usr/share/dict", "/usr/local/share/dict"].iter() {
@@ -26,77 +41,81 @@ fn open_dict() -> File {
     panic!("could not find a dictionary");
 }
 
-type Hist = multiset::HashMultiSet<char>;
+/// A histogram is represented as a multiset or "bag" of
+/// characters.
+type Histogram = multiset::HashMultiSet<char>;
 
+/// A dictionary entry maps a word to its corresponding
+/// histogram.
 struct Entry {
     word: String,
-    whist: Hist
+    whist: Histogram
 }
 
-type Dict = Vec<Entry>;
-type WordList = Vec<String>;
-
-fn hist_string(w: &str) -> Option<Hist> {
-    let mut h = Hist::new();
-    for c in w.chars() {
+/// Calculate the histogram of the given word.
+fn word_histogram(word: &str) -> Option<Histogram> {
+    let mut histogram = Histogram::new();
+    for c in word.chars() {
         if !c.is_alphabetic() {
             return None
         };
-        let ci =
+        let c_lowercase =
             if c.is_uppercase() {
-                let mut clc = c.to_lowercase();
-                let cl = clc.next().expect("character has no lowercase codes");
-                if clc.count() != 0 {
+                // It may be the case that a single uppercase
+                // char translates to several lowercase chars.
+                let mut chars_lowercase = c.to_lowercase();
+                let c_lowercase = chars_lowercase
+                    .next()
+                    .expect("character has no lowercase codes");
+                if chars_lowercase.count() != 0 {
                     return None
                 };
-                cl
+                c_lowercase
             } else {
                 c
             };
-        h.insert(ci);
+        histogram.insert(c_lowercase);
     };
-    Some(h)
+    Some(histogram)
 }
 
-fn load_dict(target: &Hist) -> Dict {
-    let mut dict: Dict = Vec::new();
-    let extra = [
-	"s",
-	"ed",
-	"er",
-	"ing",
-	"ly",
-        "i",
-        "a"
-    ];
+/// Parse the words in the dictionary into histograms.  Do
+/// some pruning along the way for efficiency.  Augment the
+/// dictionary with common stems that can be used to help
+/// construct words.
+fn load_dictionary(target: &Histogram) -> Vec<Entry> {
+    // Load in the dictionary.
+    let mut dict: Vec<Entry> = Vec::new();
     let f = open_dict();
     let r = BufReader::new(&f);
-    for l in r.lines() {
-        let w = l.expect("cannot read word from dictionary");
-	if w.len() <= 1 {
+    for line in r.lines() {
+        let word = line.expect("cannot read word from dictionary");
+	if word.len() <= 1 {
 	    continue;
         };
-        if let Some(h) = hist_string(&w) {
-            if h <= *target {
+        if let Some(whist) = word_histogram(&word) {
+            if whist.is_submultiset(target) {
                 let e = Entry {
-                    whist: h,
-                    word: w
+                    whist,
+                    word,
                 };
                 dict.push(e);
             };
         };
     };
-    for x in extra.iter() {
-        if let Some(h) = hist_string(x) {
+    // Add the stems.
+    for stem in STEMS.iter() {
+        if let Some(whist) = word_histogram(stem) {
             let e = Entry {
-	        word: String::from(*x),
-	        whist: h
+	        word: String::from(*stem),
+	        whist,
 	    };
             dict.push(e);
         } else {
             panic!("mysterious extra entry");
         };
     };
+    // Sort in order of increasing length.
     let len_order = |a: &Entry, b: &Entry| {
 	a.word.len().cmp(&b.word.len())
     };
@@ -104,8 +123,15 @@ fn load_dict(target: &Hist) -> Dict {
     dict
 }
 
-fn anagram(dict: &Dict, remaining: &mut Hist,
-           start: usize, sofar: &mut WordList) {
+/// Construct anagrams from the suffix of `dict` starting at
+/// `start`. The `remaining` characters are available for
+/// histogramming. Anagrams are pushed onto `sofar` as they
+/// are constructed. If construction is complete, display
+/// the result.
+fn anagram(dict: &Vec<Entry>, remaining: &mut Histogram,
+           start: usize, sofar: &mut Vec<String>) {
+    // Base case: All possible anagrams have been
+    // constructed. Display and return them.
     if remaining.total_elements() == 0 {
         print!("{}", sofar[0]);
         for i in 1..sofar.len() {
@@ -114,8 +140,13 @@ fn anagram(dict: &Dict, remaining: &mut Hist,
         println!();
         return;
     };
+    // Recursive case: For each entry from start to end of
+    // the dictionary, if that entry can be used to extend
+    // the anagram do so, then recursively try all
+    // completions from that point. Use do-undo to ensure
+    // that remaining is still valid.
     for i in start..dict.len() {
-        if *remaining >= dict[i].whist {
+        if dict[i].whist.is_submultiset(remaining)  {
             *remaining -= dict[i].whist.clone();
             sofar.push(dict[i].word.clone());
             anagram(dict, remaining, i, sofar);
@@ -125,13 +156,14 @@ fn anagram(dict: &Dict, remaining: &mut Hist,
     }
 }
 
+/// Run the program.
 fn main() {
     let target = args().nth(1).expect("usage: anagram <target>");
-    let mut th = match hist_string(&target) {
+    let mut th = match word_histogram(&target) {
         Some(h) => h,
         None => panic!("invalid target")
     };
-    let dict = load_dict(&th);
+    let dict = load_dictionary(&th);
     let mut sofar = Vec::new();
     anagram(&dict, &mut th, 0, &mut sofar);
 }
